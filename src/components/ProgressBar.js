440e5cd919b1c19dda4144336632f2d8beec8b9a
Kiryl Bahdanau <kirbah@gmail.com>
2025-09-16 14:39:46 +0200

    fix: progress bar was too small

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,9 +5,9 @@ const ProgressBar = ({ current, total }) => {
   const progress = Math.round((current / total) * 100);
 
   return (
-    <div className="progress mb-4" style={{ height: "10px" }}>
+    <div className="progress mb-4" style={{ height: "12px" }}>
       <div
-        className="progress-bar"
+        className="progress-bar d-flex align-items-center px-2"
         role="progressbar"
         style={{ width: `${progress}%` }}
         aria-valuenow={progress}
