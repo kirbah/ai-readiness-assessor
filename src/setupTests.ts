c9bc7330215232b6d42e9caeae501ea59b13fb67
Kiryl Bahdanau <kirbah@gmail.com>
2025-09-18 07:13:24 +0200

    fix: framer-motion mock to fix tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,34 @@
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+vi.mock("framer-motion", async () => {
+  const React = await vi.importActual("react");
+  const actual = await vi.importActual("framer-motion");
+
+  return {
+    __esModule: true,
+    ...actual,
+    AnimatePresence: ({ children }) => children,
+    motion: new Proxy(actual.motion, {
+      get: (target, key) => {
+        const component = target[key];
+        if (typeof component === "function") {
+          return React.forwardRef((props, ref) => {
+            const { ...rest } = props;
+            return React.createElement(component, { ...rest, ref });
+          });
+        }
+        return component;
+      },
+    }),
+    animate: vi.fn((from, to, options) => {
+      if (options && typeof options.onUpdate === "function") {
+        options.onUpdate(to);
+      }
+      if (options && typeof options.onComplete === "function") {
+        options.onComplete();
+      }
+      return { stop: vi.fn() };
+    }),
+  };
+});
